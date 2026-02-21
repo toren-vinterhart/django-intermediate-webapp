61a80830c1b0c30f8b5b045e9e516dd5db90cc44
toren-vinterhart <torenvinterhart@gmail.com>
2026-02-21 22:04:13 +0330

    Add RedirectView CBV examples

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('fbv-index/', views.indexView, name='fbv_index'),
-    path('cbv-index/', views.IndexView.as_view(), name='cbv_index'),
+    path('fbv-index/', views.index_view, name='fbv-index'),
+    path('cbv-index/', views.IndexView.as_view(), name='cbv-index'),
     # path('cbv-index/', TemplateView.as_view(template_name='index.html', extra_context={'name': 'John'}), name='cbv_index'),
+    path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index', permanent=True), name='redirect-to-index'),
+    path('go-to-msn', RedirectView.as_view(url='https://www.msn.com'), name='redirect-to-msn'),
+
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -5,17 +5,23 @@ from .models import Post
 # Create your views here.
 
 
-def indexView(request):
+def index_view(request):
+    """
+    A function-based view to show index page
+    """
     context = {'name': 'Sam'}
     return render(request, 'index.html', context)
 
 
 class IndexView(TemplateView):
+    """
+    A class-based view for show index page
+    """
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['name'] = 'Jack'
-        context['post'] = Post.objects.all()
+        context['posts'] = Post.objects.all()
         return context
 
