994fb20f26bf366c50ca6fffd8826a7705d138fe
toren-vinterhart <torenvinterhart@gmail.com>
2026-02-22 23:02:23 +0330

    Add test RedirectView CBV

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from django.views.generic import TemplateView, RedirectView
+# from django.views.generic import TemplateView, RedirectView
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('fbv-index/', views.index_view, name='fbv-index'),
     path('cbv-index/', views.IndexView.as_view(), name='cbv-index'),
+    path('go-to-msn/<int:pk>', views.RedirectToMSN.as_view(), name='redirect-to-msn'),
+    # path('fbv-index/', views.index_view, name='fbv-index'),
+    # path('go-to-msn', views.redirect_to_msn, name='redirect-to-msn'),
     # path('cbv-index/', TemplateView.as_view(template_name='index.html', extra_context={'name': 'John'}), name='cbv_index'),
-    path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index', permanent=True), name='redirect-to-index'),
-    path('go-to-msn', RedirectView.as_view(url='https://www.msn.com'), name='redirect-to-msn'),
-
+    # path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index', permanent=True), name='redirect-to-index'),
+    # path('go-to-msn', RedirectView.as_view(url='https://www.msn.com'), name='redirect-to-msn'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,16 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, RedirectView
 from .models import Post
 
 # Create your views here.
-
-
-def index_view(request):
-    """
-    A function-based view to show index page
-    """
-    context = {'name': 'Sam'}
-    return render(request, 'index.html', context)
 
 
 class IndexView(TemplateView):
@@ -25,3 +17,28 @@ class IndexView(TemplateView):
         context['posts'] = Post.objects.all()
         return context
 
+
+class RedirectToMSN(RedirectView):
+    """A class-based view for redirect to msn"""
+    url = 'https://www.msn.com'
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
+
+    
+''' FBV to show a template
+def index_view(request):
+    """
+    A function-based view to show index page
+    """
+    context = {'name': 'Sam'}
+    return render(request, 'index.html', context)
+'''
+
+
+''' FBV for redirect
+def redirect_to_msn(request):
+    return redirect('https://www.msn.com')
+'''
