603222d024dbe5365a48be05effa117cba2ed64a
toren-vinterhart <torenvinterhart@gmail.com>
2026-04-13 23:01:58 +0330

    Create PostSerializer and use it in postList and postDetail api-views

---
+++ core/blog/api/v1/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from blog.models import Post
+
+
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'status', 'created_date', 'published_date' ]
+        # exclude = ['status', 'updated_date']
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from blog.models import Post
+from rest_framework import status
+# from blog.models import Post
+from ...models import Post
+from .serializers import PostSerializer
 
 
 @api_view()
 def postList(request):
-    return Response("Hi, this is a testing API")
+    posts = Post.objects.filter(status=True).order_by('-published_date')
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def postDetail(request, id):
-    # post = get_object_or_404(Post, id=id)
-    return Response({'post': 'post', 'id': id})
+    post = get_object_or_404(Post, pk=id, status=True)
+    serializer = PostSerializer(post)
+    return Response(serializer.data, status=status.HTTP_404_NOT_FOUND)
+
+    # We can do this if we don't want to use get_object_or_404 function
+    # try:
+    #     post = Post.objects.get(pk=id)
+    #     # print(post.__dict__)
+    #     serializer = PostSerializer(post)
+    #     # print(serializer.__dict__)
+    #     return Response(serializer.data)
+    # except Post.DoesNotExist:
+    #     # return Response({'detail': 'post does not exists'}, status=404)
+    #     return Response({'detail': 'post does not exists'}, status=status.HTTP_404_NOT_FOUND)
