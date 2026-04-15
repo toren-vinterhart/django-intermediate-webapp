f4776399914fee55e3f815dd50ab42f16e0d1a1f
toren-vinterhart <torenvinterhart@gmail.com>
2026-04-15 22:08:28 +0330

    Implement PostList and PostDetail cbv inheriting generics and mixins APIView

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework import mixins, generics, viewsets
+# from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 # from blog.models import Post
@@ -10,7 +12,7 @@ from .serializers import PostSerializer
 
 
 # region function_base_api_view
-
+'''
 @api_view(["GET", "POST"])
 # @permission_classes([IsAdminUser])
 # @permission_classes([IsAuthenticatedOrReadOnly])
@@ -60,11 +62,11 @@ def postDetail(request, id):
     # except Post.DoesNotExist:
     #     # return Response({'detail': 'post does not exists'}, status=404)
     #     return Response({'detail': 'post does not exists'}, status=status.HTTP_404_NOT_FOUND)
-
+'''
 # endregion
 
 # region class_base_API_View
-
+'''
 class PostList(APIView):
     """ Getting a list of posts and creating new posts """
     permission_classes = [IsAuthenticated]
@@ -108,5 +110,54 @@ class PostDetail(APIView):
         post = get_object_or_404(Post, pk=id, status=True)
         post.delete()
         return Response({'detail': 'Item removed successfully!!!'}, status=status.HTTP_204_NO_CONTENT)
+'''
+# endregion
+
+# region class_base_Mixin_and_Generic_API_View
+'''
+class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.filter(status=True)
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class PostDetail(mixins.RetrieveModelMixin, 
+                 mixins.UpdateModelMixin, 
+                 mixins.DestroyModelMixin, 
+                 generics.GenericAPIView):
+    
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.filter(status=True)
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+'''
+# endregion
+
+# region class_base_generics_API_View
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.filter(status=True)
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.filter(status=True)
+    serializer_class = PostSerializer
+    # lookup_field = 'id' # If we use id in urls instead of pk we must set this
 
 # endregion
