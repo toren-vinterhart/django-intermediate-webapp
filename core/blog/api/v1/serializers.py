7d597cfd3ab2c83f1ba7e31fc70898792a6e3fa3
toren-vinterhart <torenvinterhart@gmail.com>
2026-04-19 17:12:11 +0330

    Modify PostSerializer to set author automatically

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from blog.models import Post, Category
+from accounts.models import Profile
 
 
 # class PostSerializer(serializers.Serializer):
@@ -15,8 +16,8 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     # We can set items to read_only here or in Meta class
-    # content = serializers.ReadOnlyField()
-    # content = serializers.CharField(read_only=True)
+    # author = serializers.ReadOnlyField()
+    # author = serializers.CharField(read_only=True)
     snippet = serializers.ReadOnlyField(source='get_snippet') # get_snippet is a method in Post model
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True) # get_absolute_api_url is a method in Post model
     absolute_url = serializers.SerializerMethodField(method_name='get_abs_url') # method name must be get_{field_name} e.g. get_absolute_url if we don't set method_name
@@ -27,7 +28,7 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'author', 'image', 'title', 'content', 'snippet', 'relative_url', 'absolute_url', 'category', 'status', 'created_date', 'published_date' ]
         # exclude = ['status', 'updated_date']
-        # read_only_fields = ['content']
+        read_only_fields = ['author']
 
     def get_abs_url(self, obj):
         request = self.context.get('request')
@@ -44,8 +45,11 @@ class PostSerializer(serializers.ModelSerializer):
             rep.pop('absolute_url', None)
         else:
             rep.pop('content', None)
-        rep['category'] = CategorySerializer(instance.category).data
+        rep['category'] = CategorySerializer(instance.category, context={'request': request}).data
         # rep['name'] = 'Jack' # Everything we want to show can add in this way 
         return rep
-
-
+    
+    def create(self, validated_data):
+        # validated_data['author'] = Profile.objects.get(user__id=self.context.get('request').user.id)
+        validated_data['author'] = Profile.objects.get(user=self.context.get('request').user)
+        return super().create(validated_data)
