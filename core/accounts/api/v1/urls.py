159fd2f4eecf724c5d72b64cc0f939e03e0c913f
toren-vinterhart <torenvinterhart@gmail.com>
2026-05-02 23:40:14 +0330

    Implement JWT-Authentication with SimpleJWT package

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,17 +1,29 @@
 from django.urls import path
 from . import views
 # from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 app_name = 'api-v1'
 
 urlpatterns = [
     # registration
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
+    
+    # change password
+    
+    # reset password
+    
+    # login token
+    # path('token/login/', ObtainAuthToken.as_view(), name='token-login'),
     path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
     path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout'),
-    # path('token/login/', ObtainAuthToken.as_view(), name='token-login'),
-    # change password
-    # reset password
-    # login token
+    
     # login jwt
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
 ]
